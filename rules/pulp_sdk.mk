0d7f9fe01606d31e11393b6a6459621273305a21
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2019-06-12 10:22:59 +0200

    Fixed regression

--- rules/pulp_sdk.mk
+++ rules/pulp_sdk.mk
@@ -17,5 +17,5 @@ ifdef PULPOS
 endif
 ifdef SRC
 	mkdir -p $(TARGET_INSTALL_DIR)/src/pmsis_bsp
-	cp -r bsp camera display flash fs include zephyr $(TARGET_INSTALL_DIR)/src/pmsis_bsp
+	cp -r bsp camera display flash include zephyr $(TARGET_INSTALL_DIR)/src/pmsis_bsp
 endif
