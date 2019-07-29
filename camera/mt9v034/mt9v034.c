1be3fb8e3ccafc1c3e58d417590841bade64f534
S. Sivarajah <sivarajah.sivapiriyan@greenwaves-technologies.com>
2019-07-30 01:05:54 +0200

    Update camera mt9v034 for freertos & pmsis

--- camera/mt9v034/mt9v034.c
+++ camera/mt9v034/mt9v034.c
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-
+#include "pmsis.h"
 #include "pmsis/drivers/gpio.h"
 #include "pmsis/drivers/i2c.h"
 #include "pmsis/drivers/cpi.h"
@@ -230,8 +230,6 @@ static void __mt9v034_trigger_snapshot(mt9v034_t *mt9v034)
 
 static int __mt9v034_open(struct pi_device *device)
 {
-  bsp_init();
-
   struct mt9v034_conf *conf = (struct mt9v034_conf *)device->config;
 
   mt9v034_t *mt9v034 = (mt9v034_t *)pmsis_l2_malloc(sizeof(mt9v034_t));
