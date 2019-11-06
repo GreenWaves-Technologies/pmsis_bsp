310b9e630a95a5d2f0ab6e68a7c62778550b046a
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2019-11-06 01:43:53 +0100

    Fixed issue in pi_cl_fs_copy, cluster id was missing

--- fs/read_fs/read_fs.c
+++ fs/read_fs/read_fs.c
@@ -613,6 +613,7 @@ void __pi_cl_fs_copy_req(void *_req)
     __pi_cl_fs_copy_req_done(_req);
 }
 
+
 void pi_cl_fs_copy(pi_fs_file_t *file, uint32_t index, void *buffer, uint32_t size, int32_t ext2loc, pi_cl_fs_req_t *req)
 {
   req->file = file;
@@ -621,6 +622,7 @@ void pi_cl_fs_copy(pi_fs_file_t *file, uint32_t index, void *buffer, uint32_t si
   req->size = size;
   req->ext2loc = ext2loc;
   req->length = 0;
+  req->cid = pi_cluster_id();
   #if defined(__PULP_OS__)
   __rt_task_init_from_cluster(&req->task);
   #endif  /* __PULP_OS__ */
@@ -641,6 +643,7 @@ void pi_cl_fs_copy_2d(pi_fs_file_t *file, uint32_t index, void *buffer, uint32_t
   req->stride = stride;
   req->length = length;
   req->ext2loc = ext2loc;
+  req->cid = pi_cluster_id();
   #if defined(__PULP_OS__)
   __rt_task_init_from_cluster(&req->task);
   #endif  /* __PULP_OS__ */
