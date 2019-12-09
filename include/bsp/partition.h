1b937c47e30c420e74c202ac81d289b4c90d1ba6
Mathieu Barbe <mathieu@kolabnow.com>
2019-12-09 10:24:49 +0100

    refactor (partition): Edit license

--- include/bsp/partition.h
+++ include/bsp/partition.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 GreenWaves Technologies
+ * Copyright (C) 2018 GreenWaves Technologies
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +12,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ */
+
+/*
+ * Authors: Mathieu Barbe, GreenWaves Technologies (mathieu.barbe@greenwaves-technologies.com)
  */
 
 #ifndef __BSP_PARTITION_H__
